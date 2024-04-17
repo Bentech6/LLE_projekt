b53ae56ebc7fad6c646160f45550d98567c2883d
Benedek Zsolt Balog <balogbenedekzsolt@gmail.com>
2024-04-17 11:25:14 +0200

    LLE.py update

--- LLE.py
+++ LLE.py
@@ -6,3 +6,15 @@ from matplotlib import pyplot as plt
 class LLE:
     def __init__(self):
         pass
+
+    def k_nearest(self):
+        pass
+
+    def calc_w_matrix(self):
+        pass
+
+    def cost_func_opt(self):
+        pass
+
+    def eval(self):
+        pass
